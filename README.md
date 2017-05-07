689a7f86d63577dc92b91f9c902d3a99a9fa31e7
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-05-07 08:24:16 -0500

    start function and readme

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # Green Bot
+
+Recommend use Linux
+
+### Install MySql
+
+https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-16-04
+
+### Install drivers for mysql and python and pip
+
+```
+sudo apt-get install python-pip python-dev libmysqlclient-dev
+```
+
+### Installation
+
+Clone the project
+
+```
+git clone https://github.com/JuniorZavaleta/green_bot
+```
+
+Go to the directory
+
+```
+cd green_bot
+```
+
+Create a virtual env
+```
+virtualenv venv --distribute
+```
+
+Activate the virtual env
+```
+source venv/bin/activate
+```
+
+Install the dependencies
+```
+pip install -r requirements.txt
+```
+
+### Deployment
+Use ngrok and configure your webhook
+
+Download ngrok
+
+https://ngrok.com/download
+
+Execute ngrok
+
+./path/to/ngrok http 5000
+
+Flask use port 5000 by default
+
+Copy the https url (i.e. https://abc123.ngrok.io)
+
+On bot.py
+Set your telegram token on TELEGRAM_TOKEN
+And your https url of ngrok on SITE_URL
+
+Enter to "https://your_site_url/set" for set the telegram webhook
+
+Use the bot.
--- bot.py
+++ bot.py
@@ -1,9 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+import logging
+from flask import Flask, request
+import telegram
+from telegram.ext import Updater, CommandHandler
+from emoji import emojize
 
+logging.basicConfig(level=logging.DEBUG,
+                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger()
+
+#Environment
+TELEGRAM_TOKEN = "TELEGRAM_TOKEN_BOT"
+SITE_URL = "NGROL_HTTPS"
+
+global bot, updater
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 app = Flask(__name__)
+updater = Updater(TELEGRAM_TOKEN)
+
+# Commands
+def start(bot, update):
+    print("start")
+    update.message.reply_text(u'Hola!, Soy Hojita {}'.format(
+        emojize(':grinning_face:', use_aliases=True)))
+
+# Handlers
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+# Routes
+@app.route('/handle_telegram', methods=['POST'])
+def handle_webhook():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        print(update)
+        updater.dispatcher.process_update(update)
+    return 'ok'
+
+@app.route('/set', methods=['GET', 'POST'])
+def set_webhook():
+    if bot.setWebhook(SITE_URL + '/handle_telegram'):
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
 
 @app.route('/')
 def index():
