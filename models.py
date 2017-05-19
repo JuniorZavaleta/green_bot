191e6408ae9818dd0df2c2e061e16381ec2e8836
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-05-18 23:56:43 -0500

    config env variables

--- models.py
+++ models.py
@@ -1,18 +1,8 @@
 from orator import DatabaseManager, Model
 from orator.orm import belongs_to_many
+import config
 
-config = {
-    'mysql': {
-        'driver': 'mysql',
-        'host': 'localhost',
-        'database': 'green_case',
-        'user': 'root',
-        'password': 'root',
-        'prefix': ''
-    }
-}
-
-db = DatabaseManager(config)
+db = DatabaseManager(config.DATABASE)
 Model.set_connection_resolver(db)
 
 class ContaminationType(Model):
